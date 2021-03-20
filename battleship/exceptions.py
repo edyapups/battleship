d18c4937929643f71e4ac0d9475061f31483e67c
Edgar Zhivaev <edyapups@gmail.com>
2021-03-20 16:20:04 +0300

    Added initial exceptions implementation

---
+++ battleship/exceptions.py
@@ -0,0 +1,38 @@
+# Board errors
+
+class BoardError(Exception):
+    pass
+
+
+class MoveError(BoardError):
+    pass
+
+
+class PlacementError(BoardError):
+    pass
+
+
+# Player errors
+
+class PlayerError(Exception):
+    pass
+
+
+class QuitSignal(PlayerError):
+    QUIT = 'q'
+    QUIT_AND_SAVE = 'qs'
+    BACK_TO_MAIN_MENU = 'btmms'
+
+    def __init__(self, *args: object, signal_type: str) -> None:
+        super().__init__(*args)
+        self.signal_type = signal_type
+
+
+# Game errors
+
+class GameError(Exception):
+    pass
+
+
+class GameIsEnded(Exception):
+    pass
